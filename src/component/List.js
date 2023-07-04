2d68cc3133caa597433121fd409f79471739c2a8
song <thdgusrb@gmail.com>
2023-07-04 14:12:43 +0900

    검색 기능 완성

--- src/component/List.js
+++ src/component/List.js
@@ -14,25 +14,58 @@ export default function List() {
    let navigate = useNavigate();
    var data = localStorage.getItem('DATA');
    data = JSON.parse(data);
+   let [test, setTest] = useState(data);
 
-   useEffect(()=>{
-      if(empty != 'list-container-empty'){
+   useEffect(() => {
+      if (empty != 'list-container-empty') {
          setEmpty('list-container');
       }
    }, [empty])
-   console.log(empty);
+   // console.log(empty);
 
    return (
       // <div className="list-container">
       <div className={`${empty}`}>
          <div className="list-search">
-            <input onInput={(e)=>{
+            <input onInput={(e) => {
                setSearch(e.target.value);
-            }} type="text" placeholder="한줄평 검색"></input> <button onClick={()=>{
+
+               if (e.target.value == '') {
+                  setTest(data);
+               }
+            }} type="text" placeholder="검색할 제목을 입력"></input> <button onClick={() => {
                console.log(search);
+
+               var copy = [...test];
+               var arr = copy.filter((element) => {
+                  if(element.title.includes(search)){
+                     return element
+                  }
+               })
+               setTest(arr);
+
             }}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
          </div>
          {
+            test != null ?
+               test.map(function (item, i) {
+                  return (
+                     <div onClick={() => {
+                        navigate(`/list/detail/${item.num}`);
+                     }} className="list-item" key={i}>
+                        <div>No.{item.num}</div>
+                        <div className="list-title">제목: {item.title} </div>
+                        <div className="list-content">{item.content}</div>
+                        <div className="list-writer">작성자: {item.writer}</div>
+                        <div className="list-date">{item.date}</div>
+                     </div>
+                  )
+               }) :
+               <div className="list-item" style={{ textAlign: 'center' }}>
+                  <h2>아직 작성된 글이 없습니다.</h2>
+               </div>
+         }
+         {/* {
             data != null ?
                data.map(function (item, i) {
                   return (
@@ -50,7 +83,7 @@ export default function List() {
                <div className="list-item" style={{ textAlign: 'center' }}>
                   <h2>아직 작성된 글이 없습니다.</h2>
                </div>
-         }
+         } */}
       </div>
    )
 }
--- src/component/Update.js
+++ src/component/Update.js
@@ -13,30 +13,33 @@ export default function Update() {
    let [content, setContent] = useState(data[id].content);
 
    return (
-      <div className="detail-container">
-         <div className="detail-item">
-            <div className="write-title">
-               제목<br /> <input onInput={(e) => {
-                  // console.log(e.target.value)
-                  setTitle(e.target.value)
-               }} type="text" name="title" defaultValue={data[id].title}></input>
-            </div>
-            <div className="write-content">
-               한줄평<br /> <input onInput={(e) => {
-                  // console.log(e.target.value)
-                  setContent(e.target.value)
-               }} name="content" defaultValue={data[id].content}></input>
-            </div>
-            <div className="detail-writer">작성자: {data[id].writer}</div>
-            <div className="detail-date">{data[id].date}</div>
-            <button onClick={() => {
-               data[id] = {num: data[id].num ,title: title, content: content, writer: data[id].writer, date: data[id].date}
-               console.log(data)
-               data = JSON.stringify(data);
-               localStorage.setItem('DATA', data);
-               navigate('/list');
-            }}>Update</button>
-         </div>
+      <div className="write-container">
+         <form>
+            <fieldset>
+               <legend>한줄평 수정 ✍</legend>
+               <div className="write-title">
+                  제목<br /> <input onInput={(e) => {
+                     // console.log(e.target.value)
+                     setTitle(e.target.value)
+                  }} type="text" name="title" defaultValue={data[id].title}></input>
+               </div>
+               <div className="write-content">
+                  한줄평<br /> <input onInput={(e) => {
+                     // console.log(e.target.value)
+                     setContent(e.target.value)
+                  }} name="content" defaultValue={data[id].content}></input>
+               </div>
+               <div className="detail-writer">작성자: {data[id].writer}</div>
+               <div className="detail-date">{data[id].date}</div>
+               <button className="write-save" onClick={() => {
+                  data[id] = { num: data[id].num, title: title, content: content, writer: data[id].writer, date: data[id].date }
+                  console.log(data)
+                  data = JSON.stringify(data);
+                  localStorage.setItem('DATA', data);
+                  navigate('/list');
+               }}>Update</button>
+            </fieldset>
+         </form>
       </div>
    )
 }
